81d9df1e6acb33036820270300def5fb1547f8bf
Ryan M. Lederman <lederman@gmail.com>
2023-08-13 19:27:38 -0600

    update lint.sh

--- lint.sh
+++ lint.sh
@@ -1,7 +1,29 @@
 #!/usr/bin/env bash
 
-cppcheck -j 8 --check-level=exhaustive --max-ctu-depth=4 --force --std=c11 --enable=all --language=c *.c && \
-cppcheck -j 8 --check-level=exhaustive --max-ctu-depth=4 --force --std=c++20 --enable=all --language=c++ *.cc
+if which cppcheck >/dev/null 2>&1; then
+    echo "Running cppcheck..."
+    cppcheck --check-level=exhaustive --max-ctu-depth=8 \
+             --force --std=c11 --enable=all --language=c *.c && \
+    cppcheck --check-level=exhaustive --max-ctu-depth=8 \
+             --force --std=c++20 --enable=all --language=c++ *.cc || {
+        echo "cppcheck returned error code $?; exiting"
+        exit $?
+    }
+else
+    echo "cppcheck not found; skipping"
+fi
 
-#clang-tidy -p build --use-color --header-filter=.* *.c *.cc
-#analyze-build --cdb build/compile_commands.json --use-analyzer /usr/bin/clang --stats
+if which analyze-build >/dev/null 2>&1; then
+    echo "Running clang-tidy..."
+    clang-tidy -p build --export-fixes=clang-fixes.yml \
+               --checks=clang-diagnostic-*,clang-analyzer-* *.c &&
+    clang-tidy -p build --export-fixes=clang-fixes.yml \
+               --checks=clang-diagnostic-*,clang-analyzer-*,modernize-*,readability-* *.cc || {
+        echo "clang-tidy returned error code $?; exiting"
+        exit $?
+    }
+
+else
+    echo "clang-tidy not found; skipping"
+fi
+
