8e1469c470946f7f7b93def6cdeb17d0e6a72363
Ryan M. Lederman <lederman@gmail.com>
2023-08-19 19:04:15 -0600

    try POLLRDHUP in input to poll

--- src/balinternal.c
+++ src/balinternal.c
@@ -418,6 +418,9 @@ short _bal_mask_topollflags(uint32_t mask)
 
     if (bal_isbitset(mask, BAL_EVT_PRIORITY))
         bal_setbitshigh(&retval, POLLPRI);
+#elif defined(__linux__)
+    if (bal_isbitset(mask, BAL_EVT_CLOSE))
+        bal_setbitshigh(&retval, POLLRDHUP);
 #endif
 
     return retval;
