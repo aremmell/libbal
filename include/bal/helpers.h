4f96a9dd18c919405b72f19a5575c8615aad9d38
Ryan M. Lederman <lederman@gmail.com>
2023-08-23 21:51:30 -0700

    improve bal_setbits* macros

--- include/bal/helpers.h
+++ include/bal/helpers.h
@@ -46,21 +46,10 @@ void __bal_safefree(void** pp)
 # define bal_isbitset(bitmask, bit) (((bitmask) & (bit)) == (bit))
 
 /** Sets the specified bits to one in the target bitmask. */
-# define bal_setbitshigh(pbitmask, bits) \
-    if (NULL != (pbitmask)) { \
-        (*(pbitmask)) |= (bits); \
-    }
+# define bal_setbitshigh(pbitmask, bits) ((*(pbitmask)) |= (bits))
 
 /** Sets the specified bits to zero in the target bitmask. */
-# define bal_setbitslow(pbitmask, bits) \
-    if (NULL != (pbitmask)) { \
-        (*(pbitmask)) &= ~(bits); \
-    }
-
-/** Returns the size of a sockaddr struct (IPv4/IPv6). */
-# define _BAL_SASIZE(sa) \
-    ((PF_INET6 == ((struct sockaddr* )&(sa))->sa_family) \
-        ? sizeof(struct sockaddr_in6) : sizeof(struct sockaddr_in))
+# define bal_setbitslow(pbitmask, bits) ((*(pbitmask)) &= ~(bits))
 
 /** Returns the number of entries in an array. */
 # define _bal_countof(arr) (sizeof((arr)) / sizeof((arr)[0]))
@@ -74,10 +63,15 @@ void __bal_safefree(void** pp)
 /** Used with getnameinfo: perform DNS queries. */
 # define _BAL_NI_DNS   (NI_NAMEREQD | NI_NUMERICSERV)
 
-/** Locks the specified mutex  and asserts that it was locked. */
+/** Returns the size of a sockaddr struct (IPv4/IPv6). */
+# define _BAL_SASIZE(sa) \
+    ((PF_INET6 == ((struct sockaddr* )&(sa))->sa_family) \
+        ? sizeof(struct sockaddr_in6) : sizeof(struct sockaddr_in))
+
+/** Locks the specified mutex and asserts that it was locked. */
 # define _BAL_LOCK_MUTEX(m, name) \
     bool name##_locked = _bal_mutex_lock(m); \
-    BAL_ASSERT_UNUSED(name##_locked,name##_locked)
+    BAL_ASSERT_UNUSED(name##_locked, name##_locked)
 
 /** Unlocks a mutex and asserts that it was unlocked. */
 # define _BAL_UNLOCK_MUTEX(m, name) \
