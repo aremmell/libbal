bad9112fde3f45d812e40bee95e4ef47ae377caa
Ryan M. Lederman <lederman@gmail.com>
2023-09-30 06:59:46 -0600

    WIP socket_base

--- include/bal.hh
+++ include/bal.hh
@@ -29,9 +29,11 @@
 # include "bal.h"
 # include <type_traits>
 # include <stdexcept>
+# include <cstdlib>
 # include <vector>
 # include <atomic>
 # include <string>
+# include <bit>
 
 /** The one and only namespace for libbal. */
 namespace bal
@@ -103,6 +105,12 @@ namespace bal
         explicit address(const bal_sockaddr& addr) : _sockaddr(addr) { }
         virtual ~address() = default;
 
+        address& operator=(const bal_sockaddr& addr)
+        {
+            _sockaddr = addr;
+            return *this;
+        }
+
         address_info get_address_info(bool dns_resolve = false) const
         {
             bal_addrstrings strings {};
@@ -116,6 +124,11 @@ namespace bal
         const bal_sockaddr& get_sockaddr() const
         {
             return _sockaddr;
+        }
+
+        void clear()
+        {
+            std::memset(&_sockaddr, 0, sizeof(bal_sockaddr));
         }
 
     private:
@@ -158,15 +171,64 @@ namespace bal
         }
     };
 
-    template<bool RAII>
+    class policy
+    {
+    protected:
+        policy() = default;
+        virtual ~policy() = default;
+
+    public:
+        static constexpr bool throw_on_error() noexcept {
+            return true;
+        }
+    };
+
+    class default_policy : public policy
+    {
+    public:
+        default_policy() = default;
+        ~default_policy() override = default;
+    };
+
+    template<class T>
+    concept DerivedFromPolicy = std::derived_from<T, policy>;
+
+    template<DerivedFromPolicy TPolicy, typename T>
+    inline T throw_on_policy(const T& value, const T& invalid)
+    {
+        if (value == invalid) {
+            if constexpr(TPolicy::throw_on_error()) {
+                throw exception(error::from_last_error());
+            }
+        }
+        return value;
+    }
+
+    enum class shutdown_mode : int
+    {
+        read = BAL_SHUT_RD,
+        write = BAL_SHUT_WR,
+        readwrite = BAL_SHUT_RDWR
+    };
+
+    template<bool RAII, DerivedFromPolicy TPolicy>
     class socket_base
     {
     public:
         socket_base(int addr_fam, int type, int proto) requires RAII
         {
-            if (!bal_create(&_s, addr_fam, type, proto)) {
-                throw exception(error::from_last_error());
-            }
+            const auto created = bal_create(&_s, addr_fam, type, proto);
+            [[maybe_unused]]
+            const auto excpt = throw_on_policy<TPolicy>(created, false);
+        }
+
+        socket_base(int addr_fam, int proto, const std::string& host,
+            const std::string& srv) requires RAII
+        {
+            const auto created =
+                bal_auto_socket(&_s, addr_fam, proto, host.c_str(), srv.c_str());
+            [[maybe_unused]]
+            const auto excpt = throw_on_policy<TPolicy>(created, false);
         }
 
         socket_base(socket_base&) = delete;
@@ -212,12 +274,103 @@ namespace bal
             return attach(nullptr);
         }
 
+        bool close(bool destroy = true)
+        {
+            if (!is_valid()) {
+                return false;
+            }
+
+            const auto closed = bal_close(&_s, destroy);
+            return throw_on_policy<TPolicy>(closed, false);
+        }
+
+        bool shutdown(shutdown_mode how)
+        {
+            const auto sd = bal_shutdown(_s, how);
+            return throw_on_policy<TPolicy>(sd, false);
+        }
+
+        bool connect(const std::string& host, const std::string& port)
+        {
+            const auto conn = bal_connect(_s, host.c_str(), port.c_str());
+            return throw_on_policy<TPolicy>(conn, false);
+        }
+
+        ssize_t send(const void* data, bal_iolen len, int flags)
+        {
+            const auto sent = bal_send(_s, data, len, flags);
+            return throw_on_policy<TPolicy>(sent, -1);
+        }
+
+        ssize_t sendto(const std::string& host, const std::string& port,
+            const void* data, bal_iolen len, int flags)
+        {
+            const auto sent =
+                bal_sendto(_s, host.c_str(), port.c_str(), data, len, flags);
+            return throw_on_policy<TPolicy>(sent, -1);
+        }
+
+        ssize_t recv(void* data, bal_iolen len, int flags)
+        {
+            const auto read = bal_recv(_s, data, len, flags);
+            return throw_on_policy<TPolicy>(read, -1);
+        }
+
+        ssize_t recvfrom(void* data, bal_iolen len, int flags, address& whence)
+        {
+            whence.clear();
+
+            bal_sockaddr tmp {};
+            const auto read = bal_recvfrom(_s, data, len, flags, &tmp);
+            if (read) {
+                whence = tmp;
+            }
+
+            return throw_on_policy<TPolicy>(read, -1);
+        }
+
+        bool bind(const std::string& addr, const std::string& srv)
+        {
+            const auto bound = bal_bind(_s, addr.c_str(), srv.c_str());
+            return throw_on_policy<TPolicy(bound, false);
+        }
+
+        bool bind_all(const std::string& srv)
+        {
+            const auto bound = bal_bindall(_s, srv.c_str());
+            return throw_on_policy<TPolicy(bound, false);
+        }
+
+        bool listen(int backlog = SOMAXCONN)
+        {
+            const auto listening = bal_listen(_s, backlog);
+            return throw_on_policy<TPolicy(listening, false);
+        }
+
+        bool accept(socket_base& client_sock, address& client_addr)
+        {
+            const auto existing = client_sock.detach();
+            BAL_ASSERT(existing == nullptr);
+
+            client_addr.clear();
+
+            bal_socket* s = nullptr;
+            bal_sockaddr addr {};
+            const auto accepted = bal_accept(_s, &s, &addr);
+            if (accepted) {
+                [[maybe_unused]] auto unused = client_sock.attach(s);
+                client_addr = addr;
+            }
+
+            return throw_on_policy<TPolicy>(accepted, false);
+        }
+
     private:
         bal_socket* _s = nullptr;
     };
 
-    using scoped_socket = socket_base<true>;
-    using manual_socket = socket_base<false>;
+    using scoped_socket = socket_base<true, default_policy>;
+    using manual_socket = socket_base<false, default_policy>;
 
 } // !namespace bal
 
