64a49b88e0b68bbcd5af802a4b1062a4194e3215
Ryan M. Lederman <lederman@gmail.com>
2023-09-29 20:09:47 -0600

    add (maybe throw-away) exception and address_info

--- include/bal.hh
+++ include/bal.hh
@@ -27,9 +27,88 @@
 # define _BAL_HH_INCLUDED
 
 # include "bal.h"
+# include <stdexcept>
+# include <atomic>
+# include <string>
 
 namespace bal
 {
+    // TODO: capture extended error info and code, expose those.
+    class exception : public std::runtime_error
+    {
+    public:
+        using std::runtime_error::runtime_error;
+        explicit exception(const bal_error& err) : exception(err.message) { }
+
+        static exception from_last_error() {
+            bal_error err {};
+            [[maybe_unused]] int code = bal_get_error(&err);
+            return exception(err);
+        }
+    };
+
+    template<bool DNS>
+    class address_info_base
+    {
+    public:
+        address_info_base() = default;
+        explicit address_info_base(const bal_sockaddr& addr) {
+            from_sockaddr(addr);
+        }
+        virtual ~address_info_base() = default;
+
+        address_info_base& operator=(const bal_sockaddr& addr) {
+            return from_sockaddr(addr);
+        }
+
+        address_info_base& from_sockaddr(const bal_sockaddr& addr) {
+            bal_addrstrings strings {};
+            if (!bal_get_addrstrings(&addr, DNS, &strings)) {
+                throw exception::from_last_error();
+            }
+
+            _type = strings.type;
+            _host = strings.host;
+            _addr = strings.addr;
+            _port = strings.port;
+
+            return *this;
+        }
+
+        std::string get_type() const {
+            return _type;
+        }
+
+        std::string get_host() const {
+            return _host;
+        }
+
+        // TODO: ability to return sockaddr
+        std::string get_addr() const {
+            return _addr;
+        }
+
+        // TODO: ability to return uint16_t
+        std::string get_port() const {
+            return _port;
+        }
+
+        void clear() {
+            _type.clear();
+            _host.clear();
+            _addr.clear();
+            _port.clear();
+        }
+
+    private:
+        std::string _type;
+        std::string _host;
+        std::string _addr;
+        std::string _port;
+    };
+
+    using dns_address_info = address_info_base<true>;
+    using address_info = address_info_base<false>;
 
 } // !namespace bal
 
