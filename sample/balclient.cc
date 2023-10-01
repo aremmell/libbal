4a769028bebe071452072a00f67ed6ca04884078
Ryan M. Lederman <lederman@gmail.com>
2023-09-30 21:05:15 -0600

    remove superfluous includes

--- sample/balclient.cc
+++ sample/balclient.cc
@@ -23,9 +23,6 @@
  * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-#include <iostream>
-#include <iomanip>
-#include <exception>
 #include <array>
 #include "balcommon.hh"
 
