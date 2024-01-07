9b11678c56a42776281de34d6a9c14fa6739ed27
Ryan M. Lederman <lederman@gmail.com>
2024-01-07 03:37:47 -0700

    ...

--- include/bal/platform.h
+++ include/bal/platform.h
@@ -278,7 +278,7 @@ typedef unsigned bal_threadret;
 # include <assert.h>
 
 # define BAL_MAXERROR       256
-# define BAL_MAXERRORDETAIL 27 + 10
+# define BAL_MAXERRORDETAIL 256
 # define BAL_MAXERRORFMT    BAL_MAXERROR + BAL_MAXERRORDETAIL
 # define BAL_UNKNOWN "<unknown>"
 
